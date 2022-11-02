1de5526f7875ed62407ad55c731bb9311b80fa46
mextriner <mextrinerx1998@gmail.com>
2022-11-02 12:59:03 +0100

    Primera Actualizacion
    Clase Autor con su AutorDao;
    Conexión y Manejo de Archivos

---
+++ ProyectoBiblioteca/src/main/java/Dominio/Autor.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dominio;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class Autor {
+    private int idautor;
+    private String nombre;
+    private String apellido;
+    private String nacionalidad;
+    private Date fechaNac;
+
+    public Autor() {
+    }
+
+    public Autor(String nombre, String apellido, String nacionalidad, Date fechaNac) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nacionalidad = nacionalidad;
+        this.fechaNac = fechaNac;
+    }
+
+    
+    
+    public Autor(int idautor, String nombre, String apellido, String nacionalidad, Date fechaNac) {
+        this.idautor = idautor;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nacionalidad = nacionalidad;
+        this.fechaNac = fechaNac;
+    }
+
+    public int getIdautor() {
+        return idautor;
+    }
+
+    public void setIdautor(int idautor) {
+        this.idautor = idautor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    @Override
+    public String toString() {
+        return "Autor{" + "idautor=" + idautor + ", nombre=" + nombre + ", apellido=" + apellido + ", nacionalidad=" + nacionalidad + ", fechaNac=" + fechaNac + '}';
+    }
+    
+    
+    
+}
---
+++ ProyectoBiblioteca/src/main/java/com/ceep/proyectobiblioteca/TesrMain.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ceep.proyectobiblioteca;
+
+import Dominio.Autor;
+import Dominio.AutorDao;
+import ManejoArchivos.ManejoDeArchivos;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author Maximo
+ */
+public class TesrMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    
+    public static AutorDao autorDao = new AutorDao();
+    
+    public static List <Autor> autores(){
+        List <Autor> aut = null;
+        try{
+            List <Autor> persona = autorDao.seleccionar();
+            aut = persona;
+//            personas.forEach(persona->{
+//                System.out.println("persona = "+persona); 
+//            });
+        }catch(SQLException ex){
+            ex.printStackTrace(System.out);
+        }
+        return aut;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Date date = new Date(70,4,5);
+        Autor autor = new Autor(1,"Patrick","Rothfuss","EEUU",date);
+//        
+//        
+//        autorDao.actualizar(autor);
+
+        Date date1 = new Date(98,6,28);
+        Autor autor1 = new Autor("Máximo","Mestriner","España",date1);
+ //       autorDao.insertar(autor1);
+ 
+        //Recorro el List de autores y añado a traves del id los autores en el archivp de texto
+
+        for (int i = 1; i <= autores().size(); i++) {
+            ManejoDeArchivos.agregarArchivo("autores.txt","%"+autor(i).toString());
+        }
+
+        
+        
+    }
+    
+     public static Autor autor(int id){
+        Autor nAutor = null;
+        List <Autor> lista = autores();
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getIdautor() == id){
+                nAutor = lista.get(i);
+            }
+        }
+            
+        return nAutor;
+    }
+    
+}

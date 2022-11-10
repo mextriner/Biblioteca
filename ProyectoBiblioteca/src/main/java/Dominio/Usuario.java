086f369aa56b375044fd41ba2ef189eb3444d01b
mextriner <mextrinerx1998@gmail.com>
2022-11-10 14:24:27 +0100

    actualizacion del id usuario

--- ProyectoBiblioteca/src/main/java/Dominio/Usuario.java
+++ ProyectoBiblioteca/src/main/java/Dominio/Usuario.java
@@ -6,45 +6,57 @@
 package Dominio;
 
 import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 /**
  *
  * @author Alumno Mañana
  */
 public class Usuario {
-    private int idusuario;
-    private String nombre;
+    private String usuario;
     private String clave;
+    private Timestamp fechaAlt;
+    private String nombre;
     private String apellido;
     private String direccion;
     private Date fechaNac;
 
-    public Usuario(int idusuario, String nombre, String apellido, String direccion, Date fechaNac) {
-        this.idusuario = idusuario;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.direccion = direccion;
-        this.fechaNac = fechaNac;
-    }
-
     public Usuario() {
     }
 
-    public Usuario(String nombre, String apellido, String direccion, Date fechaNac) {
+    public Usuario(String usuario, String clave) {
+        this.usuario = usuario;
+        this.clave = clave;
+        this.fechaAlt = Timestamp.valueOf(LocalDateTime.now());
+    }
+
+    
+    
+    public Usuario(String usuario, String clave, String nombre, String apellido, String direccion, Date fechaNac) {
+        
+        this.usuario = usuario;
+        this.clave = clave;
         this.nombre = nombre;
         this.apellido = apellido;
         this.direccion = direccion;
         this.fechaNac = fechaNac;
     }
-    
-    
 
-    public int getIdusuario() {
-        return idusuario;
+    public String getUsuario() {
+        return usuario;
     }
 
-    public void setIdusuario(int idusuario) {
-        this.idusuario = idusuario;
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
     }
 
     public String getNombre() {
@@ -53,6 +65,10 @@ public class Usuario {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public Timestamp getFechaAlt() {
+        return fechaAlt;
     }
 
     public String getApellido() {
@@ -81,12 +97,14 @@ public class Usuario {
 
     @Override
     public String toString() {
-        return "Usuario{" + "idusuario=" + idusuario + ", nombre=" + nombre + ", apellido=" + apellido + ", direccion=" + direccion + ", fechaNac=" + fechaNac + '}';
+        return "Usuario{" + "usuario=" + usuario + ", clave=" + clave + ", fechaAlt=" 
+                + fechaAlt + ", nombre=" + nombre + ", apellido=" + apellido 
+                + ", direccion=" + direccion + ", fechaNac=" + fechaNac + '}';
     }
     
-    
     public String escribir() {
-        return '%' + '*' + idusuario + '*' + nombre + '*' + apellido + '*' + direccion + '*' + fechaNac + '*' ;
+        return '%' + '*' + usuario + '*' + clave + '*' + fechaAlt + '*' + nombre
+                + '*' + apellido + '*' + '*' + direccion + '*' + '*' + fechaNac + '*' ;
     }
     
     
--- ProyectoBiblioteca/src/main/java/Dominio/UsuarioDao.java
+++ ProyectoBiblioteca/src/main/java/Dominio/UsuarioDao.java
@@ -12,6 +12,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +23,14 @@ import java.util.List;
 public class UsuarioDao {
 
     private static final String SQL_SELECT ="SELECT * FROM idUsuario";
-    private static final String SQL_INSERT = "INSERT INTO usuario (nombre,"
+    
+    private static final String SQL_DECRYPT = "SELECT usuario, "
+            + "CAST(AES_DECRYPT('?','key'))AS clave FROM idUsuario";
+    
+    private static final String SQL_INSERT = "INSERT INTO usuario (usuario,"
+            + "clave) VALUES (?,CAST(AES_ENCRYPT('?','key')))";
+    
+    private static final String SQL_INSERT_ALL = "INSERT INTO usuario (nombre,"
             + "apellido,direccion, fechaNac) VALUES (?,?,?,?)";
     
     private static final String SQL_UPDATE = "UPDATE usuario SET "
@@ -30,9 +38,9 @@ public class UsuarioDao {
             + "apellido = ?,"
             + "direccion = ?,"
             + "fechaNac = ?"
-            + "WHERE idUsuario = ?";
+            + "WHERE usuario = ?";
     
-    private static final String SQL_DELETE = "DELETE FROM usuario WHERE idUsuario = ?";
+    private static final String SQL_DELETE = "DELETE FROM usuario WHERE usuario = ?";
     
 //    Método que nos lista todas las personas de nuestro sistema
     public List<Usuario> seleccionar() throws SQLException {
@@ -49,14 +57,45 @@ public class UsuarioDao {
         rs = stmt.executeQuery();
         
         while(rs.next()){
-            int idusuario = rs.getInt("idUsuario");
+            String usuarioId = rs.getString("usuario");
+            String clave = rs.getString("clave");
             String nombre = rs.getString("nombre");
             String apellido = rs.getString("apellido");
             String direccion = rs.getString("direccion");
             Date fechaNac = rs.getDate("fechaNac");
             
             //Instancio un nuevo objeto
-            usuarios.add(new Usuario(idusuario, nombre,apellido,direccion,fechaNac));
+            usuarios.add(new Usuario(usuarioId, clave,nombre,apellido,direccion,fechaNac));
+            
+        }
+        
+        
+        close(rs);
+        close(stmt);
+        close(conn);
+        
+        return usuarios;
+    }
+    
+    public List<Usuario> verClaves() throws SQLException {
+        //INICIALIZAR VARIABLES
+        
+        Connection conn = null;
+        PreparedStatement stmt= null;
+        ResultSet rs = null;
+        Usuario usuario = null;
+        List<Usuario> usuarios = new ArrayList<>();
+        
+        conn = getConnection();
+        stmt = conn.prepareStatement(SQL_DECRYPT);
+        rs = stmt.executeQuery();
+        
+        while(rs.next()){
+            String usuarioId = rs.getString("usuario");
+            String clave = rs.getString("aes_decrypt(clave,'key')as char");
+            
+            //Instancio un nuevo objeto
+            usuarios.add(new Usuario(usuarioId, clave));
             
         }
         
@@ -86,10 +125,54 @@ public class UsuarioDao {
             
             //3. ASIGNAR LOS VALORES A LOS INTERROGANTES DE LA CONSULTA
             
-            stmt.setString(1, usuario.getNombre());
-            stmt.setString(2, usuario.getApellido());
-            stmt.setString(3, usuario.getDireccion());
-            stmt.setDate(4, usuario.getFechaNac());
+            stmt.setString(1, usuario.getUsuario());
+            stmt.setString(2, usuario.getClave());
+            
+            
+            //4. EJECUTO LA QUERY
+            
+            registro = stmt.executeUpdate();
+            
+            
+        }catch(SQLException ex){
+            ex.printStackTrace(System.out);
+        }finally{
+            try{
+                close(stmt);
+                close(conn);
+            }catch(SQLException ex){
+                ex.printStackTrace(System.out);
+            
+            }
+            
+        }
+        return registro;
+    }
+    
+    
+    public int insertarAll (Usuario usuario){
+        Connection conn =null;
+        PreparedStatement stmt=null;
+        int registro = 0;
+        
+        try{
+             //1. ESTABLECER CONEXIÓN
+        
+            conn = getConnection();
+            
+            //2. PREPARO LA INSTRUCCIÓN EJECUTABLE EN MYSQL
+            
+            stmt = conn.prepareStatement(SQL_INSERT_ALL);
+            
+            
+            //3. ASIGNAR LOS VALORES A LOS INTERROGANTES DE LA CONSULTA
+            
+            stmt.setString(1, usuario.getUsuario());
+            stmt.setString(2, usuario.getClave());
+            stmt.setString(3, usuario.getNombre());
+            stmt.setString(4, usuario.getApellido());
+            stmt.setTimestamp(5, usuario.getFechaAlt());
+            stmt.setDate(6, usuario.getFechaNac());
             
             
             //4. EJECUTO LA QUERY
@@ -133,7 +216,7 @@ public class UsuarioDao {
             stmt.setString(2, usuario.getApellido());
             stmt.setString(3, usuario.getDireccion());
             stmt.setDate(4, usuario.getFechaNac());
-            stmt.setInt(5, usuario.getIdusuario());
+            stmt.setString(5, usuario.getUsuario());
             
             
             //4. EJECUTO LA QUERY
@@ -173,7 +256,7 @@ public class UsuarioDao {
             
             //3. ASIGNAR LOS VALORES A LOS INTERROGANTES DE LA CONSULTA
             
-            stmt.setInt(1, usuario.getIdusuario());
+            stmt.setString(1, usuario.getUsuario());
             
             
             //4. EJECUTO LA QUERY
